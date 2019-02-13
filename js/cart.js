2d2209e679db8626ef382ef829dd19609554af53
Anastasia-Zhang <1003814986@qq.com>
2019-02-13 14:31:06 +0800

    Render the list of items using the v-for directive

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,19 @@
 var vm = new Vue({
     el:"#app",
     data:{
-        title
+        totalMoney:0,
+        productList:[]
+    },
+    mounted:function(){
+      this.cartView();
     },
     methods:{
-
+        cartView:function () {
+            var _this = this;//将this存入变量代表本实例，在方法中的this作用域将发生变化
+            this.$http.get("data/cartData.json",{"id":"123"}).then(function (res) {
+                _this.productList = res.data.result.list;//将接口返回的list数据放到vue的list里面
+                _this.totalMoney = res.data.result.totalMoney;
+            });
+        }
     }
 });
